8e7e4bca3d531fb4bc4e3855dd3dafd0e41737bb
Kyleah Frederic <krfreder@live.com>
2020-03-31 12:42:14 -0500

    Completed bonus missions

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  itemBeingEdited: object = null;
+  
+  equipment: object[] = [
+    {name: "Habitat Dome"},
+    {name: "Drones"},
+    {name: "Food Containers"},
+    {name: "Oxygen Tanks"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+  
+  add(itemName: string) {
+    this.equipment.push({name: itemName});
+  }
+  
+  remove(item: object) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+  
+  edit(item: object) {
+    this.itemBeingEdited = item;
+  }
+
+  save(name: string, item: object) {
+   item['name'] = name;
+   this.itemBeingEdited = null;
+  }
 
 }
+
+
+
+
+
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  
+  researchBeingEdited: object = null;
+    
+    experiments: object[] = [
+      {name: "Mars soil sample"},
+      {name: "Plant growth in habitat"},
+      {name: "Human bone density"},
+    ];
 
-  constructor() { }
-
-  ngOnInit() {
+    constructor() { }
+    
+    ngOnInit() {
+  }
+  
+  add(researchName: string) {
+    this.experiments.push({name: researchName});
+  }
+  
+  remove(research: object) {
+    let index = this.experiments.indexOf(research);
+    this.experiments.splice(index, 1);
+  }
+  
+  edit(research: object) {
+    this.researchBeingEdited = research;
+  }
+  
+  save(name: string, research: object) {
+   research['name'] = name;
+   this.researchBeingEdited = null;
   }
 
 }
+
+
+  
